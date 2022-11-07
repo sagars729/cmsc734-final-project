2da3d65702e613dc156a720d199878807468e789
Harsh Patel <hpatel01@umd.edu>
2022-11-07 10:04:31 -0500

    can add and remove elements

--- src/components/KeyPointsList/KeyPointsList.tsx
+++ src/components/KeyPointsList/KeyPointsList.tsx
@@ -1,3 +1,4 @@
+import moment from 'moment';
 import React, { useState} from 'react';
 import './KeyPointsList.css';
 // import key_points from "../../json_data.json";
@@ -30,88 +31,110 @@ const UserInput = () => {
         ]
     }]
     
+    //place holder values. these will be taken from shubham
+    let counter = 1;
+
     const [data, setData] = useState(originalData);
 
     const printData = (data:any) => {
         console.log(data);
     }
     
-    const addKeyPoint = () => {
+    const addKeyPoint = (time:string, attribute:string) => {
+        let newData = [...data];
+
+        //temporarily adding fake dates
+        var i = moment()
+        .utcOffset('+08:30')
+        .format('MM-DD-YY hh:mm:ss');
         
-    
+
+        var jsonPoints = [{ "variable": attribute, "point_value": "" + counter, "analysis_yielded": "<input>"}]
+        var jsonObj = { "time": i, "points": jsonPoints};
+
+        newData.push(jsonObj);
+        newData.sort( (a, b) => a.time.localeCompare(b.time));
+        setData(newData);        
+    }
+
+    // don't think we'll need this
+    const componentDidUpdate = (id:any) => {
+        var element = document.getElementById(id);
+        if (element != null){
+            element.scrollIntoView();
+            element.scrollIntoView({behavior: "smooth"}); 
+        }
     }
     
     const deletePoint = (time:string , index:number) => {
-        console.log(time);
-        console.log(index);
-
-        console.log("data length =" + data.length);
 
         let newData = [...data];
 
         newData.find(val => {return val.time === time})?.points.splice(index, 1);
         var len = newData.find(val => {return val.time === time})?.points.length;
-        console.log("array length =" + len);
-        // var kk = jsonObj?.points.splice(index, 1);
+        
         if (len === 0) {
-            console.log("0000");    
             var toRemove = newData.findIndex(val => {return val.time === time});
-            console.log("index to remove " + toRemove);
             newData.splice(toRemove, 1);
             // console.log(data);
+
+            newData.sort( (a, b) => a.time.localeCompare(b.time));
         }
         
-        setData(newData);
-        
+        setData(newData);        
 
     }
     
     return (
         <div>
-                <div className="centered">
-                    <h1> Key Points </h1>
-                    {data.map(item => {
-                        return (
-                            <div key={item.time}>
-                                <h4>{item.time}</h4>
-                                <div>
-                                {item.points.map( (point, i) => {
-                                    return (
-                                        <div key={i}>
-                                            <p>value: {point.point_value}</p>
-                                            <p>Variable: {point.variable}</p>
-                                            <textarea 
-                                                defaultValue={point.analysis_yielded}
-                                                onChange={(e) => {
-                                                    point.analysis_yielded = e.target.value;
-                                                }}>   
-                                            </textarea>
-                                            <div>
-                                                <button onClick={ (e) => deletePoint(item.time, i)}>
-                                                    delete key_point
-                                                </button>
-                                            </div>
+            <div className="centered">
+                <h1> Key Points </h1>
+                {data.map(item => {
+                    return (
+                        <div id={item.time} key={item.time} className="parent">
+                            <h4>{item.time}</h4>
+                            <div>
+                            {item.points.map( (point, i) => {
+                                return (
+                                    <div key={i} className="child">
+                                        <p>value: {point.point_value}</p>
+                                        <p>Variable: {point.variable}</p>
+                                        <textarea 
+                                            defaultValue={point.analysis_yielded}
+                                            onChange={(e) => {
+                                                point.analysis_yielded = e.target.value;
+                                            }}>   
+                                        </textarea>
+                                        <div>
+                                            <button onClick={ (e) => deletePoint(item.time, i)}>
+                                                delete key_point
+                                            </button>
                                         </div>
-                                    )
-                                })}
-                                </div>
+                                    </div>
+                                )
+                            })}
                             </div>
-                        )
-                    })}
-                    
-                    
-                    
-                </div>
-                <div className="bottom">
-                    <button onClick={(e) => printData(data)}>
-                        submit data!
-                    </button>
-                </div>
-                <div className="bottom">
-                    <button onClick={addKeyPoint}>
-                        add key_point
-                    </button>
-                </div>
+                        </div>
+                    )
+                })}
+                
+                <button onClick={(e) => {
+                        var timeStamp = moment()
+                        .utcOffset('+08:30')
+                        .format('MM-DD-YYYY hh:mm:ss');
+
+                        var variableStr = "something";
+                        addKeyPoint(timeStamp, variableStr);
+                    } }>
+
+                    add key_point
+                </button>
+                <button onClick={(e) => printData(data)}>
+                    submit data!
+                </button>
+                
+            </div>
+
                 
         </div>
     )
